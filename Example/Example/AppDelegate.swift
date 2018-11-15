4ab2e21ad9dd1989cf0b933326719b7c5ca14c34
John Lima <john.limadeveloper@icloud.com>
2018-11-15 15:17:22 -0200

    Setup project struct

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -1,9 +1,9 @@
 //
 //  AppDelegate.swift
-//  Demo
+//  Example
 //
-//  Created by John Lima on 11/11/18.
-//  Copyright © 2018 jlimadeveloper. All rights reserved.
+//  Created by John Lima on 15/11/18.
+//  Copyright © 2018 limadeveloper. All rights reserved.
 //
 
 import UIKit
@@ -13,9 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
-  // swiftlint:disable discouraged_optional_collection
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     return true
   }
 }
+
